ad080c7c65068f933fdc0faa1e8827c065444241
339780520 <339780520@2260I-L29-008>
2025-09-03 09:29:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12_T1_The_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
