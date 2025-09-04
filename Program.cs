8c80f30b73ceeb4b0344d54a3fda4ec508d35925
339780520 <339780520@2260I-L29-008>
2025-09-04 09:36:29 -0400

    started assignment questions

--- Program.cs
+++ Program.cs
@@ -50,3 +50,21 @@
         }
     }
 }
+
+
+
+//Assignment
+
+/* 1. Provide two different ways to write a greeting to someone who's name is stored in a string variable firstName.
+ * Use string interpolation and the + operator. Which method do you prefer? 
+
+* There are two ways to write something stored in a string variable. The first is to use the + operator.
+ * It connects two strings together. The second is to use string interpolation. It is done by placing a $ 
+ * before the string, and then placing the variable in curly braces {}. I prefer string interpolation because it's more neat. */
+
+/* 2. How would you print out the number of characters in a string called passPhrase?
+ * Console.WriteLine(passPhrase.Length); */
+
+/* 3. What does the Trim() method do? Why might this be useful?
+ * The Trim() method removes spaces before and after a string. This is useful because sometimes the spaces go unnoticed
+ * and it is easier to remove them than to find them. */
