b5a141abc0b808ba47f82c7d38995fd9c0a157fd
339780520 <339780520@2260I-L29-008>
2025-09-04 08:52:04 -0400

    finished replace for real this time

--- Program.cs
+++ Program.cs
@@ -35,6 +35,8 @@
             Console.WriteLine(sayHello);
             sayHello = sayHello.Replace("Hello", "Greetings");
             Console.WriteLine(sayHello);
+            Console.WriteLine(sayHello.ToUpper());
+            Console.WriteLine(sayHello.ToLower());
         }
     }
 }
