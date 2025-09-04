16d7f98c44ac403405707d3fbad951a79275ed1a
339780520 <339780520@2260I-L29-008>
2025-09-04 08:49:56 -0400

    Finished replace

--- Program.cs
+++ Program.cs
@@ -6,16 +6,35 @@
         {
             //Kylie
 
+            //Friend variables
             string aFriend = "Maira";
             string firstFriend = "Maria";
             string secondFriend = "Sage";
 
+            //Greeting variables
+            string greeting = "     Hello, World!       ";
+            string trimmedGreeting = greeting.TrimStart();
+            trimmedGreeting = greeting.Trim();
+
+            //Replace variables
+            string sayHello = "Hello, World!";
+
+            //Friends
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello " + aFriend);
             Console.WriteLine($"Hello {aFriend}");
             Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
             Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters");
             Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters");
+
+            //Greetings
+            Console.WriteLine($"[{greeting}]");
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            //Replace
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
         }
     }
 }
