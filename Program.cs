770eb6d7a55a26910a23c9cc6059ce04525f843b
339780520 <339780520@2260I-L29-008>
2025-09-04 08:40:42 -0400

    part way through the first tutorial

--- Program.cs
+++ Program.cs
@@ -5,7 +5,17 @@
         static void Main(string[] args)
         {
             //Kylie
+
+            string aFriend = "Maira";
+            string firstFriend = "Maria";
+            string secondFriend = "Sage";
+
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello " + aFriend);
+            Console.WriteLine($"Hello {aFriend}");
+            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters");
+            Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters");
         }
     }
 }
